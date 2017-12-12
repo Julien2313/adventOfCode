d0f65231b18482e73f96889e9872fc08eb2bc998
Julien2313 <julien@lorang.net>
2017-12-12 10:11:40 +0100

    Day 12

--- day12/main.py
+++ day12/main.py
@@ -1,9 +1,42 @@
-
-def p2():
-    return -1
-
+import re
 
 def p1():
-    return -2
+    data = []
+    for prog in day12Input.splitlines():
+        data.append(re.split(', | <-> ', prog.strip()))
 
-day11Input = open("day12/input.txt", "r").read().strip()
+    groupsConnected = [0]
+    getConnections(0, data, groupsConnected)
+
+    return len(groupsConnected)
+
+def getConnections(numProg, data, groupsConnected):
+    progs = []
+    for prog in data[numProg][1:]:
+        progs.append(int(prog))
+
+    for prog in progs:
+        if not prog in groupsConnected:
+            groupsConnected += [prog]
+            getConnections(prog, data, groupsConnected)
+
+def p2():
+    data = []
+    for prog in day12Input.splitlines():
+        data.append(re.split(', | <-> ', prog.strip()))
+
+    connections = [-1]*len(data)
+
+    for numProg in xrange(0, len(data)):
+        if not connections[numProg] == -1:
+            continue
+
+        groupsConnected = [numProg]
+        getConnections(numProg, data, groupsConnected)
+
+        for group in groupsConnected:
+            connections[group] = numProg
+
+    return len(set(connections))
+
+day12Input = open("day12/input.txt", "r").read().strip()
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 #http://adventofcode.com/2017/
-import day11.main as prob
+import day12.main as prob
 
 print prob.p1()
-print prob.p1_()
 print prob.p2()
-print prob.p2_()
