b890d7fbf617f04b9834fea7acb3ff3e78641431
Julien2313 <julien@lorang.net>
2018-11-30 21:32:55 -0800

    1

--- 2018/day1/main.py
+++ 2018/day1/main.py
@@ -1,23 +1,20 @@
 def p2():
-    tot = 0
-    lenCap = len(dayInput)
-    haslfLenCap = lenCap/2
-    for i in xrange(0, len(dayInput) - 1):
-        if dayInput[i] == dayInput[(i+haslfLenCap)%lenCap]:
-            tot = tot + int(dayInput[i])
+    freq=0
+    freqs={}
+    while True:
+        for data in dayInput.splitlines():
+            freq += int(data)
+            if freq in freqs:
+                return freq
+            else:
+                freqs[freq] = 1
 
-    return tot
 
 def p1():
-    tot = 0
-    for i in xrange(0, len(dayInput) - 1):
-        if dayInput[i] == dayInput[i+1]:
-            tot = tot + int(dayInput[i])
+    freq=0
+    for data in dayInput.splitlines():
+        freq += int(data)
+    return freq
 
-    if dayInput[0] == dayInput[len(dayInput) - 1]:
-        tot = tot + int(dayInput[0])
-
-    return tot
-
-dayTxt = open("day1/input.txt", "r").strip()
-dayInput = dayTxt.read()
+dayTxt = open("day1/input.txt", "r")
+dayInput = dayTxt.read().strip()
