763f5a9d333026c4377317bdbf393c91263f9df0
Julien2313 <julien@lorang.net>
2018-12-02 21:46:24 -0800

    impr

--- 2018/day3/main.py
+++ 2018/day3/main.py
@@ -1,8 +1,11 @@
 from collections import Counter
 
+X = 1000
+Y = 1000
+
 def p2():
-    playground = [[0 for x in range(1000)] for y in range(1000)]
-    allIDs = [x+1 for x in range(1293)]
+    playground = [[0 for x in xrange(X)] for y in xrange(Y)]
+    allIDs = [x+1 for x in xrange(1293)]
     for line in dayInput.splitlines():
         ID = int(line.split(' ')[0][1:])
         x1 = int(line.split(' ')[2].split(',')[0])
@@ -24,7 +27,7 @@ def p2():
     return allIDs
 
 def p1():
-    playground = [[0 for x in range(1000)] for y in range(1000)] 
+    playground = [[0 for x in xrange(X)] for y in xrange(Y)] 
     for line in dayInput.splitlines():
         x1 = int(line.split(' ')[2].split(',')[0])
         y1 = int(line.split(' ')[2].split(',')[1][:-1])
@@ -34,10 +37,8 @@ def p1():
             for y in xrange(y2):
                 playground[x1+x][y1+y]+=1
     cpt = 0
-    for x in xrange(1000):
-        for y in xrange(1000):
-            if playground[x][y] > 1:
-                cpt +=1
+    for x in xrange(X):
+        cpt += sum(i > 1 for i in playground[x])
     return cpt
         
 dayFile = open("day3/input.txt", "r")
